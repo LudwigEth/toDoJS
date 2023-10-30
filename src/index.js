959bcd0b934ce827bd005c87ade97fd064d2c074
LudwigEth <ludwig.eth@gmail.com>
2023-10-30 21:46:40 +0100

    Function for opening and closing dialog modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import './style.css';
+import './styles.css';
 
 const buttonSettings = document.getElementById('btn-settings');
 const dialogSettings = document.getElementById('dialog-settings-tasks');
 
 buttonSettings.addEventListener('click', e => {
+    if (isDialogOpen(dialogSettings)) {
+        closeDialog(dialogSettings);
+    } else {
+        openDialog(dialogSettings);
+    };
+});
+
+dialogSettings.addEventListener('click', e => {
+    const dialogDimensions = dialogSettings.getBoundingClientRect();
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        dialogSettings.close();
+    };
 });
 
 function openDialog(elementName) {
@@ -13,3 +30,8 @@ function openDialog(elementName) {
 function closeDialog(elementName) {
     elementName.close();
 };
+
+function isDialogOpen(elementName) {
+    return getComputedStyle(elementName).display !== 'none';
+};
+
