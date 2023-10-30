82c5a78a187c78f8afb25dfc7e8676e661817a02
LudwigEth <ludwig.eth@gmail.com>
2023-10-30 14:15:32 +0100

    new build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css';
+
+const buttonSettings = document.getElementById('btn-settings');
+const dialogSettings = document.getElementById('dialog-settings-tasks');
+
+buttonSettings.addEventListener('click', e => {
+});
+
+function openDialog(elementName) {
+    elementName.show();
+};
+
+function closeDialog(elementName) {
+    elementName.close();
+};
