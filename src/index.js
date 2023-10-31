3c2a04acd55ef3fe0719384e2f7a989b831fc030
LudwigEth <ludwig.eth@gmail.com>
2023-10-31 19:16:00 +0100

    Add functions to open and close settings-dialog element on click

--- src/index.js
+++ src/index.js
@@ -1,43 +1,52 @@
-import './style.css';
-const buttonSettings = document.getElementById('btn-settings');
-const dialogSettings = document.getElementById('dialog-settings-tasks');
-const wrapperContent = document.getElementById('wrapper-content');
 
-buttonSettings.addEventListener('click', e => {
-    if (isDialogOpen(dialogSettings)) {
-        fadeOut(dialogSettings);
-        closeDialog(dialogSettings);
-    } else {
-        openDialog(dialogSettings);
-    };
-});
+const settingsButton = document.getElementById('btn-settings');
+const settingsDialog = document.getElementById('dialog-settings-tasks');
 
-document.addEventListener('click', e => {
-    handleOutsideClick(e, dialogSettings);
-});
 
-function openDialog(elementName) {
-    elementName.show();
+function addClickEventListener(element, callback) {
+    element.addEventListener('click', callback);
 };
 
-function closeDialog(elementName) {
-    elementName.close();
+function removeClickEventListener(element, callback) {
+    element.removeEventListener('click', callback);
 };
 
-function isDialogOpen(elementName) {
-    return getComputedStyle(elementName).display !== 'none';
+function isDialogOpen(dialogElement) {
+    return getComputedStyle(dialogElement).display !== 'none';
 };
 
-function handleOutsideClick(e, elementName) {
-    if (e.target !== elementName && e.target !== buttonSettings) {
-        fadeOut(elementName);
-        closeDialog(elementName);
-    };
-};
-
-function fadeOut(elementName) {
-    elementName.classList.add('dialog-fade-out');
+function dialogFadeOutAnimation(dialogElement) {
+    dialogElement.classList.add('dialog-fade-out');
     setTimeout(() => {
-        elementName.classList.remove('dialog-fade-out');
-    }, 200);
+        dialogElement.classList.remove('dialog-fade-out');
+    }, 300);
 };
+
+
+
+function outsideDialogClickHandler(event, dialogElement, openDialogButton) {
+    if (event.target !== dialogElement && event.target !== openDialogButton) {
+        dialogFadeOutAnimation(dialogElement);
+        dialogElement.close();
+        removeClickEventListener(document, documentOpenSettingsDialogClickHandler);
+    };
+};
+
+function settingsButtonClickHandler(event) {
+    if (isDialogOpen(settingsDialog)) {
+        dialogFadeOutAnimation(settingsDialog);
+        settingsDialog.close();
+        event.stopPropagation();
+        removeClickEventListener(document, documentOpenSettingsDialogClickHandler);
+    };
+    if (!isDialogOpen(settingsDialog)) {
+        settingsDialog.show();
+        addClickEventListener(document, documentOpenSettingsDialogClickHandler);
+    };
+};
+
+function documentOpenSettingsDialogClickHandler(event) {
+    outsideDialogClickHandler(event, settingsDialog, settingsButton);
+};
+
+addClickEventListener(settingsButton, settingsButtonClickHandler);
