4469e1308b2f119856557381d145e6a4744975dd
LudwigEth <ludwig.eth@gmail.com>
2023-12-10 00:34:11 +0100

    new build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 import { navbar, navbarMenuEvents, navbarToDoEvents } from './modules/navbarBottom';
+import { closeSideMenuOnOutsideClick, collapseColor, collapseLanguage, setUserColorTheme, sideMenu, toggleCollapse, toggleDarkMode, userColorTheme } from './modules/sideMenu';
 import { loadToDoListFromLocalStorage } from './modules/toDoItem';
 import './style.css';
 
@@ -7,17 +8,50 @@ import './style.css';
 //     console.log('test');
 // });
 
+export const userConfig = {
+    primaryColor: 'rgb(255, 209, 220',
+    // primaryColorDomElem: sideMenu.primaryColorChoice.querySelector('.primary-pink'),
+    secondaryColor: 'rgb(184, 216, 184',
+    // secondaryColorDomElem: sideMenu.secondaryColorChoice.querySelector('.primary-green'),
+    darkMode: false,
+};
+
+export function saveUserConfigToLocalStorage() {
+    localStorage.setItem('userConfig', JSON.stringify(userConfig));
+};
+
+export function getUserConfigFromLocalStorage() {
+    const storedConfig = localStorage.getItem('userConfig');
+    if (storedConfig) {
+        const storedUserConfig = JSON.parse(storedConfig);
+        userConfig.primaryColor = storedUserConfig.primaryColor;
+        userConfig.secondaryColor = storedUserConfig.secondaryColor;
+        userConfig.darkMode = storedUserConfig.darkMode;
+    } else {
+        return;
+    };
+};
+
 window.addEventListener('load', () => {
     const loadingScreen = document.getElementById('loadingScreen');
     if (loadingScreen) {
         setTimeout(() => {
             loadingScreen.style.display = 'none';
         }, 1500);
-    }
+    };
 });
 
+sideMenu.menu.addEventListener('click', closeSideMenuOnOutsideClick);
+sideMenu.darkmodeButton.addEventListener('click', toggleDarkMode);
+sideMenu.primaryColorChoice.addEventListener('click', userColorTheme);
+sideMenu.secondaryColorChoice.addEventListener('click', userColorTheme);
+sideMenu.dropdownLanguage.addEventListener('click', toggleCollapse);
+sideMenu.dropdownColor.addEventListener('click', toggleCollapse);
 
 navbar.todo.addEventListener('click', navbarToDoEvents);
 loadToDoListFromLocalStorage();
+getUserConfigFromLocalStorage();
+setUserColorTheme(userConfig.primaryColor, userConfig.secondaryColor);
 
 navbar.menu.addEventListener('click', navbarMenuEvents);
+console.log(userConfig);
